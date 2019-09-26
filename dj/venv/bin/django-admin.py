3e0e08b66f6aa10c8ac4033e826b34b6cc1be181
Opeyemi Openiyi <doubleope@gmail.com>
2019-09-26 16:14:13 -0500

    flask test

--- dj/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/ope/Documents/Projects/RGBGUI/dj/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
---
+++ testapp.py
@@ -0,0 +1,52 @@
+from flask_influxdb import InfluxDB
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+influx_db = InfluxDB(app=app)
+
+
+@app.route('/')
+def index():
+    return "Hello"
+
+
+@app.route('/newdb/<dbname>')
+def newdb(dbname):
+    dbcon = influx_db.connection
+    dbcon.create_database(dbname)
+    return 'created'
+
+
+@app.route('/write/<dbname>')
+def write(dbname):
+    data_measurement = 'testseries'
+    data_tags = ['time', 'value_1', 'value_2', 'value_3']
+
+    dbcon = influx_db.connection
+    dbcon.switch_database(database=dbname)
+    dbcon.write_points([
+        {
+            "fields": {
+                'value_1': 0.5,
+                'value_2': 1,
+                'value_3': 1.8858
+            },
+
+            "measurement": "testseries"
+        }
+    ])
+    tabledata = dbcon.query('SELECT {0} from {1}'.format(', '.join(data_tags), data_measurement))
+
+    data_points = []
+    for measurement, tags in tabledata.keys():
+        for p in tabledata.get_points(measurement=measurement, tags=tags):
+            data_points.append(p)
+
+    return render_template('table.html',
+                           measurement=data_measurement,
+                           columns=data_tags,
+                           points=data_points)
+
+
+app.run()
