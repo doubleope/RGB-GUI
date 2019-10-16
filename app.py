a3845afdf39b4bb8801eb7802767e86b01b68dfa
Opeyemi Openiyi <doubleope@gmail.com>
2019-10-16 18:52:53 -0500

    remove test route

--- app.py
+++ app.py
@@ -18,35 +18,7 @@ def newdb(dbname):
     return 'created'
 
 
-@app.route('/write/<dbname>')
-def write(dbname):
-    data_measurement = 'testseries'
-    data_tags = ['time', 'value_1', 'value_2', 'value_3']
 
-    dbcon = influx_db.connection
-    dbcon.switch_database(database=dbname)
-    dbcon.write_points([
-        {
-            "fields": {
-                'value_1': 0.5,
-                'value_2': 1,
-                'value_3': 1.8858
-            },
-
-            "measurement": "testseries"
-        }
-    ])
-    tabledata = dbcon.query('SELECT {0} from {1}'.format(', '.join(data_tags), data_measurement))
-
-    data_points = []
-    for measurement, tags in tabledata.keys():
-        for p in tabledata.get_points(measurement=measurement, tags=tags):
-            data_points.append(p)
-
-    return render_template('table.html',
-                           measurement=data_measurement,
-                           columns=data_tags,
-                           points=data_points)
 
 
 app.run()
