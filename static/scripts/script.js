bff40d9c9e39fb3471182aecbe5ca407fc45a269
Opeyemi Openiyi <doubleope@gmail.com>
2019-11-22 14:07:44 -0600

    add new js file

---
+++ static/scripts/script.js
@@ -0,0 +1,13 @@
+document.getElementById("result").innerHTML;
+
+
+function update_values() {
+    $.getJSON($SCRIPT_ROOT + '/getInfo',
+          
+  function(data) {
+    $('#result').text(data.result);
+    console.log(data)
+  });
+  
+};
+
