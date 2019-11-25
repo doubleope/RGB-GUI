90fc4dbf34f576e1089a35c6b44701f5ea3078f4
Opeyemi Openiyi <doubleope@gmail.com>
2019-11-25 16:16:40 -0600

    format indentation for JSON output

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -2,7 +2,7 @@ $(function() {
     $('a#process').bind('click', function() {
       $.getJSON('/getInfo', function(data) {
         console.log(data);
-        $("#result").text(JSON.stringify(data));
+        $("#result").text(JSON.stringify(data, undefined, 10));
       });
       return false;
     });
