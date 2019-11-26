2f5fb34240f8163fc1975bd74f3f75e4acf6a860
Opeyemi Openiyi <doubleope@gmail.com>
2019-11-26 16:35:29 -0600

    json output is now presented nicely and properly formatted

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -3,6 +3,9 @@ $(function() {
       $.getJSON('/getInfo', function(data) {
         console.log(data);
         $("#result").text(JSON.stringify(data, undefined, 10));
+        $(document).ready(function() {
+          $('#result').each(function(i, e) {hljs.highlightBlock(e)});
+       });
       });
       return false;
     });
