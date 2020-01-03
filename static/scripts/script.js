115d4dfb7a6c926184f069433ff6356d1a424cf6
Opeyemi Openiyi <doubleope@gmail.com>
2020-01-02 22:29:26 -0600

    address javascript conflict issue

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,24 +1,26 @@
-$(function() {
-    $('a#process').bind('click', function() {
-      $.getJSON('/getInfo', function(data) {
+var j = jQuery_1_4_2
+
+j(function() {
+  j('a#process').bind('click', function() {
+    j.getJSON('/getInfo', function(data) {
         console.log(data);
-        $("#result").text(JSON.stringify(data, undefined, 10));
-        $(document).ready(function() {
-          $('#result').each(function(i, e) {hljs.highlightBlock(e)});
+        j("#result").text(JSON.stringify(data, undefined, 10));
+        j(document).ready(function() {
+          j('#result').each(function(i, e) {hljs.highlightBlock(e)});
        });
       });
       return false;
     });
   });
 
-  $(function() {
-    $('a#save').bind('click', function() {
-    $.getJSON('/postClusterInfo', {
-      cluster_name: $('input[name="cluster_name"]').val(),
-      cluster_type: $('input[name="cluster_type"]').val(),
-      ip: $('input[name="ip"]').val(),
-      port: $('input[name="port"]').val(),
-      mac_address: $('input[name="mac_address"]').val()
+  j(function() {
+    j('a#save').bind('click', function() {
+      j.getJSON('/postClusterInfo', {
+      cluster_name: j('input[name="cluster_name"]').val(),
+      cluster_type: j('input[name="cluster_type"]').val(),
+      ip: j('input[name="ip"]').val(),
+      port: j('input[name="port"]').val(),
+      mac_address: j('input[name="mac_address"]').val()
     }, function() {
       return false
     });
