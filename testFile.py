b3928bc37fee7fb2b6d93862bdf0947368c82d44
Opeyemi Openiyi <doubleope@gmail.com>
2020-01-04 17:10:02 -0600

    add new way of handling data

--- testFile.py
+++ testFile.py
@@ -23,3 +23,16 @@ data_points = []
 for measurement, tags in dbData.keys():
     for p in dbData.get_points(measurement=measurement, tags=tags):
         data_points.append(p)
+
+
+
+dbData_points = list(dbData.get_points(measurement = 'clusters'))
+final_points = []
+
+for i in dbData_points:
+    final_points.append(i)
+
+
+
+
+temp = ResultSet({'('clusters', None)': [{'time': '2020-01-03T05:39:42.88922125Z', 'cluster_name': 'Node1'}]})
