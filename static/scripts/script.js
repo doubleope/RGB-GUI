4298b8c55885f20755438db21a4038f43ddfdc26
Opeyemi Openiyi <doubleope@gmail.com>
2020-01-16 17:23:26 -0600

    cluster type dropdown

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -23,7 +23,7 @@ j(function() {
   j('a#save').bind('click', function() {
     j.getJSON('/postClusterInfo', {
     cluster_name: j('input[name="cluster_name"]').val(),
-    cluster_type: j('input[name="cluster_type"]').val(),
+    cluster_type: j('#type').find('option:selected').text(),
     ip: j('input[name="ip"]').val(),
     port: j('input[name="port"]').val(),
     mac_address: j('input[name="mac_address"]').val()
